9df49c96b867ab2de909e4b04d7c56af922bcf8d
HeapArt <74390692+HeapArt@users.noreply.github.com>
2022-12-29 22:10:55 -0500

    fix indents from 4 to 2

--- testScripts/servo_test.py
+++ testScripts/servo_test.py
@@ -5,7 +5,7 @@ pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 while 1:
-    wInput = input("Enter Servo No and Position : ")
-    wInputArr = wInput.split()
-    pwm.set_pwm(int(wInputArr[0]), 0, int(wInputArr[1]))
-    time.sleep(3)
+  wInput = input("Enter Servo No and Position : ")
+  wInputArr = wInput.split()
+  pwm.set_pwm(int(wInputArr[0]), 0, int(wInputArr[1]))
+  time.sleep(3)
